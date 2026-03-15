ca7a7a51acec7ffd2b1dcd20868a1e9abb18c3d9
James Kekeli <jameskekeli12@gmail.com>
2026-03-15 22:50:15 +0000

    feature:brand and category models

---
+++ phone_brands/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PhoneBrandsConfig(AppConfig):
+    name = 'phone_brands'
---
+++ phone_brands/models.py
@@ -0,0 +1,10 @@
+'''Docstring'''
+from django.db import models
+
+# Create your models here.
+class PhoneBrand(models.Model):
+    '''Docstring'''
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    added_at = models.DateTimeField(auto_now_add=True)

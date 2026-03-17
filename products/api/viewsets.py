d54acf44380648629d2339fd74ef273552d0a6b0
James Kekeli <jameskekeli12@gmail.com>
2026-03-17 23:05:11 +0000

    update:api view optimization for filters

--- products/api/viewsets.py
+++ products/api/viewsets.py
@@ -1,7 +1,9 @@
+'''Docstring'''
 from rest_framework.viewsets import ModelViewSet
-from products.serializers import ProductSerializer
-from products.models import Product
+from products.serializers import ProductSerializer #pylint:disable=e0401
+from products.models import Product #pylint:disable=e0401
 
 class ProductViewset(ModelViewSet):
+    '''Documentation String'''
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
--- products/serializers.py
+++ products/serializers.py
@@ -1,11 +1,14 @@
+'''Documentation String'''
 from rest_framework import serializers
 from categories.serializers import CategorySerializer
 from brands.serializers import BrandSerializer
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    '''Documentation String'''
     category = CategorySerializer(read_only=True)
     brand = BrandSerializer(read_only=True)
     class Meta:
+        '''Documentation String'''
         model = Product
         fields = "__all__"

6628298e1e9ddcf10c66819c5d25bc6772e1b74a
James Kekeli <jameskekeli12@gmail.com>
2026-03-19 20:03:57 +0000

    feat:rendering images and image path from api

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ from rest_framework_simplejwt.views import (
 )
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,6 @@ urlpatterns = [
     path('api/v1/',include('brands.urls')),
     path('api/v1/',include('products.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/api/viewsets.py
+++ products/api/viewsets.py
@@ -2,6 +2,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
+from rest_framework.parsers import MultiPartParser,FormParser
 from products.serializers import ProductSerializer #pylint:disable=e0401
 from products.models import Product #pylint:disable=e0401
 from products.filters import ProductFilter #pylint:disable=e0401
@@ -15,3 +16,5 @@ class ProductViewset(ModelViewSet):
 
     filter_backends = [DjangoFilterBackend]
     filterset_class = ProductFilter
+
+    parser_classes = (MultiPartParser, FormParser)

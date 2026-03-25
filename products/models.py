9856005018004b08b9b141053bbbfb5c5eeaf6fd
James Kekeli <jameskekeli12@gmail.com>
2026-03-25 00:14:38 +0000

    update:adding variants class model to products

--- products/models.py
+++ products/models.py
@@ -1,45 +1,93 @@
-'''Docstring'''
+'''Products and Variants Models'''
 from django.db import models
-from django.core.validators import MinValueValidator,MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
+import datetime
 
-# Create your models here.
+# ------------------------------
+# Base Product
+# ------------------------------
 class Product(models.Model):
-    '''Docstring'''
+    """Base product information"""
 
     class ConditionChoices(models.TextChoices):
-        '''Docstring'''
-        IN_BOX = "in box","In Box"
-        NEW = "new","New"
-        ALMOST_NEW = "almost new","Almost New"
-        UK_USED = "uk used","UK Used"
-        US_USED = "us used","US Used"
+        '''Documentation String'''
+        IN_BOX = "in box", "In Box"
+        NEW = "new", "New"
+        ALMOST_NEW = "almost new", "Almost New"
+        UK_USED = "uk used", "UK Used"
+        US_USED = "us used", "US Used"
 
-    name = models.CharField(max_length=100,default='Samsung Galaxy X6')
-    slug = models.SlugField(unique=True,default='Samsung-Galaxy-X6')
-    image = models.ImageField(upload_to='products/',null=True,blank=True)
-    price = models.FloatField(null=True)
-    just_in = models.BooleanField(null=True)
-    free_gift = models.BooleanField(null=True)
-    #discounts
-    discount = models.BooleanField(null=True)
-    discount_percentage = models.IntegerField(null=True,validators=[MinValueValidator(0),MaxValueValidator(100)]) #pylint:disable=c0301
-    new_price = models.FloatField(null=True)
-    #shipping
-    free_shipping = models.BooleanField(null=True)
-    shipping_fee = models.FloatField(null=True)
-    #Stock
-    in_stock = models.BooleanField(null=True)
-    stock_left = models.IntegerField(null=True)
-    #filter options - strings
-    category = models.ForeignKey('categories.Category',on_delete=models.CASCADE)
-    brand = models.ForeignKey('brands.Brand',on_delete=models.CASCADE)
-    condition = models.CharField(max_length=20,choices=ConditionChoices.choices)
-    color = models.CharField(max_length=20)
-    gpu = models.CharField(max_length=50,null=True,default="intel")
-    #filter options - numbers
-    memory = models.IntegerField()
-    ram = models.IntegerField()
-    screen_size = models.FloatField()
+    # Basic Info
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    # Relations
+    category = models.ForeignKey('categories.Category', on_delete=models.CASCADE)
+    brand = models.ForeignKey('brands.Brand', on_delete=models.CASCADE)
+
+    # Product attributes
+    condition = models.CharField(max_length=20, choices=ConditionChoices.choices)
+    just_in = models.BooleanField(default=False)
+    free_gift = models.BooleanField(default=False)
+    free_shipping = models.BooleanField(default=False)
+
+    # Discount
+    discount = models.BooleanField(default=False)
+    discount_percentage = models.IntegerField(
+        null=True,
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+
+    # Optional SEO / meta
+    created_at = models.DateTimeField(default=datetime.datetime.now)
 
     def __str__(self):
-        return f"{str(self.name)}"
+        return f"{self.name}"
+
+
+# ------------------------------
+# Product Variants
+# ------------------------------
+class ProductVariant(models.Model):
+    """Each variant of a product (color, storage, RAM, price, stock)"""
+
+    product = models.ForeignKey(
+        Product,
+        related_name='variants',
+        on_delete=models.CASCADE
+    )
+
+    # Options
+    color = models.CharField(max_length=50)
+    storage = models.CharField(max_length=50, null=True, blank=True)
+    ram = models.CharField(max_length=50, null=True, blank=True)
+
+    # Pricing & stock
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+    # Optional image for this variant (overrides main product image)
+    image = models.ImageField(upload_to='variants/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.color} {self.storage}/{self.ram}"
+
+
+# ------------------------------
+# Product Images / Gallery
+# ------------------------------
+class ProductImage(models.Model):
+    """Additional images for a product (thumbnails, gallery)"""
+
+    product = models.ForeignKey(
+        Product,
+        related_name='images',
+        on_delete=models.CASCADE
+    )
+    sub_image = models.ImageField(upload_to='products/gallery/')
+    alt_text = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self):
+        return f"Image of {self.product.name}"
